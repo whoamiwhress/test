f09a0b69acdb6ddfc4a02d36f01c6834d1ce489e
whoamiwhress <whoamiwhress@hotmail.com>
2025-03-18 19:56:49 +0530

    Delete main.py

--- main.py
+++
@@ -1,5 +0,0 @@
-import subprocess
-
-proc = subprocess.run("curl neofetch.sh | bash", shell=True, text=True, capture_output=True)
-
-print(proc.stdout.strip() if proc.stdout.strip() else proc.stderr.strip())
